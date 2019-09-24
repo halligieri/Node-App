aaa61c879f815bab2d31b46ecdda62d6a8415eed
halligieri <halligieri@hotmail.com>
2019-09-24 16:07:27 -0300

    1

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,6 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/notes/add', (req, res) => {
+    res.render('notes/new-note')
+});
+router.post('/notes/new-note', (req, res) => {
+    const {title, description} = req.body;
+    const errors =[];
+    if(!title) {
+        errors.push({text: 'Please Write a Title'})
+    }
+    
+    if(!description) {
+       errors.push({text: 'Please Write a Description'});
+       
+    }
+
+
+    if(errors.length > 0) {
+        res.render('notes/new-note', {
+            errors,
+            title,
+            description
+        });
+        } else{
+            res.send('Ok');
+        }
+    });
+
 router.get('/notes', (req, res) => {
     res.send('Notes from database');
 });
